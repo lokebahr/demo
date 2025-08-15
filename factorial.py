f5dddb1d6906e2cd225eca5257f0d7c32351d6bb
Loke Bahr <loketjike@gmail.com>
2025-08-15 16:20:31 +0200

    test

--- factorial.py
+++ factorial.py
@@ -1,10 +1,14 @@
+from typing import Union
 
-def factorial(n: int) -> int:
-   
-    if n == 1:
+
+def factorial(n: Union[int, float, str, None]) -> int:
+    """Calculate the factorial of a positive integer n."""
+    if not isinstance(n, int) or n < 0:
+        raise TypeError("Input must be a non-negative integer.")
+    if n in (0, 1):
         return 1
-    
     return n * factorial(n - 1)
 
+
 if __name__ == "__main__":
-    print(factorial(5))  
+    print(factorial(5))
